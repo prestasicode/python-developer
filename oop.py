1fb509c79ea2c39870fbb3588789bb39ebfc53f1
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-04-28 11:22:38 +0700

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-#create function -> define argument = population
+#create function -> define argument = population then create conditions if else
 def count_living_per_year(population: list[tuple[int, int]]) -> dict[int, int]:
   living_per_year = {}
   for birth, death in population:
@@ -14,3 +14,17 @@ def count_living_per_year(population: list[tuple[int, int]]) -> dict[int, int]:
     living_per_year[year] += living_per_year.get(year - 1, 0)
   living_per_year.pop(max(living_per_year))
   return living_per_year
+
+#create function -> define argument = population; then using concept collections defaultdict
+#define max min 
+def count_living_per_year(population: list[tuple[int, int]]) -> dict[int, int]:
+  living_per_year = collections.defaultdict(int)
+  for birth, death in population:
+    living_per_year[birth] += 1
+    living_per_year[death] -= 1
+  min_ , max_ = min(living_per_year), max(living_per_year)
+  for year in range(min_, max_):
+    living_per_year[year] += living_per_year[year - 1]
+  del living_per_year[min_ - 1], living_per_year[max_])
+  return living_per_year
+
